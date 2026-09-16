010ce9d5f794a24c05c7d0cb43dfb5eb34bba751
mxritzgit <moritz.gietl@gmail.com>
2026-09-16 02:54:58 +0200

    test(auth): scroll signup action into view in lifecycle flow

--- test/flows/lebenszyklus_flow_test.dart
+++ test/flows/lebenszyklus_flow_test.dart
@@ -119,6 +119,8 @@ void main() {
       find.byKey(const ValueKey('auth-password-field')),
       'eatova123',
     );
+    await tester.ensureVisible(find.byKey(const ValueKey('auth-submit')));
+    await tester.pumpAndSettle();
     await tester.tap(find.byKey(const ValueKey('auth-submit')));
     await settleFrames(tester);
 
